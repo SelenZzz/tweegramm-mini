ebf810ab0d7d32d84d78c603a5168db291ff09c7
Ермак Александр <ayermak@protonmail.ch>
2022-04-13 15:40:56 +0300

    add autofocus on input

--- client/src/pages/Chat/components/Input/Input.tsx
+++ client/src/pages/Chat/components/Input/Input.tsx
@@ -1,7 +1,7 @@
 import styles from './Input.module.css';
 
 // react
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // redux
 import { useDispatch } from 'react-redux';
@@ -12,8 +12,12 @@ import { SendRounded } from '@material-ui/icons';
 
 export const Input = () => {
   const dispatch = useDispatch();
-
+  const inputRef = useRef<HTMLInputElement>(null);
   const [input, setInput] = useState<string>('');
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
   const sendMessage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
@@ -26,7 +30,13 @@ export const Input = () => {
     <div className={styles.container}>
       <div className={styles.input}>
         <form>
-          <input placeholder="Message..." type="text" value={input} onChange={(event) => setInput(event.target.value)} />
+          <input
+            ref={inputRef}
+            placeholder="Message..."
+            type="text"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
           <button className={styles.send} onClick={(event) => sendMessage(event)}>
             <SendRounded />
           </button>
